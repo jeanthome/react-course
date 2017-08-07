b522f5ab91b7a430d2995df0ce939367c2701b25
Jean Thome <jtc.jean@gmail.com>
2017-08-07 19:06:32 -0300

    Primeiro commit

---
+++ ReduxSimpleStarter/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        {title: 'Javascript: To Good Parts', pages:101},
+        {title: 'Harry Potter', pages: 39},
+        {title: 'The Dark Tower', pages: 112},
+        {title: 'Eloquent Ruby', pages: 1}
+    ];
+}
